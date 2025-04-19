3a11cef3f86698216e3e32a3254624e56de92685
bmenounos <menounos@unbc.ca>
2025-04-19 13:50:39 -0700

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Initialize the map
-const map = L.map('map').setView([34.149513, -116.710434 ], 13); // Default coordinates
+// Initialize the map with a default view
+const map = L.map('map').setView([33, -116], 13); // Default coordinates
 
 // Load and add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
